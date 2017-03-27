3616d448a823403529e9591aa101ff960ba0cb3c
vijay4057 <vijaykumar4057@gmail.com>
2017-03-27 07:58:10 +0530

    customer validation

--- src/main/resources/static/customer/customer-controller.js
+++ src/main/resources/static/customer/customer-controller.js
@@ -1,3 +1,9 @@
 app.controller('CustomerController', function($scope) {
 	$scope.customerName = "Customer Name is John";
+	
+	$scope.submitForm = function(formValid){
+		alert(formValid);
+	}  
+		
+	
 });
--- src/main/resources/static/js/route.js
+++ src/main/resources/static/js/route.js
@@ -1,5 +1,8 @@
 app.config(function($routeProvider) {
     $routeProvider
+    .when("/", {
+        templateUrl : "/",
+    })
     .when("/customer", {
         templateUrl : "/customer/customer.html",
         controller : "CustomerController"
